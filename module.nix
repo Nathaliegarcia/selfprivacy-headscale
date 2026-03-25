e207b6db21a5859cdbde79ddbdcc1e0ae81707c9
Nathaliegarcia <nathalie.garcia.nce@gmail.com>
2026-03-26 00:18:17 +0100

    Update module.nix

--- module.nix
+++ module.nix
@@ -124,10 +124,6 @@ in
           proxyPass = "http://127.0.0.1:8098";
           proxyWebsockets = true;
           extraConfig = ''
-            proxy_set_header Host $server_name;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
             proxy_redirect http:// https://;
             proxy_buffering off;
           '';
